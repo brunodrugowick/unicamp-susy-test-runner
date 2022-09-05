c77906e2e6bedc308dff2cd20a909bb3414a66bd
Bruno Drugowick <brunodrugowick@users.noreply.github.com>
2022-09-05 12:45:57 -0300

    Minimal PR workflow

---
+++ test/lab02.py
@@ -0,0 +1,10 @@
+# Reading random stuff from the test files 
+D1 = int(input())
+V1 = int(input())
+T = int(input())
+D2 = int(input())
+V2 = int(input())
+
+# Printing True will match some expections, some don't... it's helpful for tests
+print(True)
+
